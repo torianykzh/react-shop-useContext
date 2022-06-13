acd546a18eace58894abf8714040b7beb44c5c34
Torianyk <torianyk.zh@gmail.com>
2022-06-13 19:34:36 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header (){
     return <nav className="green darken-1">
         <div className="nav-wrapper">
-          <a href="#!" className="brand-logo">React Shop</a>
+          <a href="/" className="brand-logo">React Shop</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="#!">Repo</a></li>
+            <li><a href="https://github.com/torianykzh/react-shop" target="_blank">Repo</a></li>
           </ul>
         </div>
     </nav>
